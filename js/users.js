f87a80ae1a2834565078429e5d93470945ff1a76
Sowbagyaa_Baskaran <sowrithis.30@gmail.com>
2021-10-07 13:10:37 +0530

    Added filter to search user

--- js/users.js
+++ js/users.js
@@ -13,7 +13,7 @@ document.getElementById("userDetailsButton").addEventListener("click",function g
   
   function displayResponse(json) {
     
-    let table="<tr><th>id</th><th>name</th><th>username</th><th>email</th><th>city</th><th>Phone</th><th>Company</th><th>Website</th></tr>";
+    let table="<thead><tr><th>id</th><th>name</th><th>username</th><th>email</th><th>city</th><th>Phone</th><th>Company</th><th>Website</th></tr></thead>";
     for (let i = 0; i < json.length; i++) { 
       table += "<tr ><td>" +json[i]["id"]+"</td><td>" +json[i]["name"] +"</td><td>" +json[i]["username"]+
       "</td><td>" +json[i]["email"]+"</td><td>" +json[i]["address"]["city"]+"</td><td>" +json[i]["phone"]+
@@ -25,19 +25,53 @@ document.getElementById("userDetailsButton").addEventListener("click",function g
     var result = document.getElementById("demo");
     if(result.style.display == "block"){
         document.getElementById("demo").style.display = "none";
+        document.getElementById("search-bar").style.display = "none";
         document.getElementById("userDetailsButton").innerHTML = "Get User Details";
     }
     else{
         document.getElementById("demo").style.display = "block";
     document.getElementById("demo").innerHTML = table;
+    document.getElementById("search-bar").style.display = "block";
     document.getElementById("userDetailsButton").innerHTML = "Close Details";
     }
     
   }
 
-  const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-  const inputValue = searchInput.value;
-  alert(inputValue);
-});
+  (function(document) {
+    'use strict';
+
+    var TableFilter = (function(myArray) {
+        var search_input;
+
+        function _onInputSearch(e) {
+            search_input = e.target;
+            var tables = document.getElementsByClassName(search_input.getAttribute('data-table'));
+            myArray.forEach.call(tables, function(table) {
+                myArray.forEach.call(table.tBodies, function(tbody) {
+                    myArray.forEach.call(tbody.rows, function(row) {
+                        var text_content = row.textContent.toLowerCase();
+                        var search_val = search_input.value.toLowerCase();
+                        row.style.display = text_content.indexOf(search_val) > -1 ? '' : 'none';
+                    });
+                });
+            });
+        }
+
+        return {
+            init: function() {
+                var inputs = document.getElementsByClassName('search-input');
+                myArray.forEach.call(inputs, function(input) {
+                    input.oninput = _onInputSearch;
+                });
+            }
+        };
+    })(Array.prototype);
+
+    document.addEventListener('readystatechange', function() {
+        if (document.readyState === 'complete') {
+            TableFilter.init();
+        }
+    });
+
+})(document);
+
